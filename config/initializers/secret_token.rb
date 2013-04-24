3bb284f6219efb6ef72f08b521e64e6d3951f2af
Michael DeVerna <michaeldeverna@yahoo.com>
2013-04-24 00:21:06 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialcal::Application.config.secret_token = '97f3bb8da4af7a7af3dfdb7a532157dce05501d11d39b8e7ea82551a57729092990b136cd1c7664d21edaa289d4156794ef8ed7b6cd476707f5cc679b075b8c9'
